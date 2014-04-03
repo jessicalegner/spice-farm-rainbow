89ed4f5fa2e95a578b676b4c35dc829fb37f951f
Jessica Legner <jessicalegner@gmail.com>
2014-04-03 00:30:09 -0400

    Trying pic2shop. Probably not going to work.

--- app/views/forms/addSpice.blade.php
+++ app/views/forms/addSpice.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.default')
+<SCRIPT LANGUAGE="JavaScript">
+     function tryscan() {
+      setTimeout(function() {
+      // if pic2shop not installed yet, go to App Store
+      window.location = "https://itunes.apple.com/us/app/pic2shop-barcode-scanner-qr/id308740640";
+     }, 25);
+     // launch pic2shop
+     window.location="pic2shop://scan?callback=http%3A//jessicalegner.com/new/%3DEAN%26";
+     }
+ </SCRIPT>
 @section('content')
 
     {{ Form::open(array('route' => array('handleAddSpice'), 'class' => 'form-horizontal')) }}
@@ -8,7 +18,10 @@
                 {{ Form::label('name', '', array('class' => 'control-label')) }}
             </div>
             <div class="col-sm-3">
-                {{ Form::text('name', '', array('class' => 'form-control', 'placeholder' => 'Name of Item')) }}
+                {{ Form::text('name', '', array('class' => 'form-control', 'placeholder' => 'Name of Item')) }} 
+            </div>
+            <div class="col-sm-1">
+                 <a href="pic2shop://scan?callback=http%3A//jessicalegner.com/new/%3DEAN%26" onclick="tryscan" class="btn btn-primary"><i class="fa fa-barcode"></i></a>
             </div>
         </div>
         <div class="form-group">
